1acc114c5e2734175c33a2deb9f7abfff02e9d72
Ronny Mundt <r.mundt@gmx.net>
2024-03-26 08:31:26 +0100

    migrate to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { StudentListControlComponent } from './components/student-list-control/student-list-control.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [StudentListControlComponent]
 })
-export class AppComponent {
-  title = 'Student List Demo';
-}
+export class AppComponent { }
--- src/app/components/student-list-control/student-list-control.component.ts
+++ src/app/components/student-list-control/student-list-control.component.ts
@@ -1,70 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { IStudent } from '../../interfaces/student.interface';
-import { StudentService } from '../../services/student.service';
+import { IStudent } from '../../interfaces';
+import { StudentService } from '../../services';
+import { NgFor, AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'student-list-control',
   templateUrl: './student-list-control.component.html',
-  styleUrls: ['./student-list-control.component.scss']
+  styleUrls: ['./student-list-control.component.scss'],
+  standalone: true,
+  imports: [NgFor, AsyncPipe]
 })
 export class StudentListControlComponent implements OnInit {
-    // PRIVATES
-    private _currentSelected: string = 'id';
-    private _isAscSorted: boolean = true;
+  private _currentSelected: string = 'id';
+  private _isAscSorted: boolean = true;
 
-    // PUBLICS
-    tableData$: Observable<IStudent[]> = new Observable<IStudent[]>();
+  tableData$: Observable<IStudent[]> = new Observable<IStudent[]>();
 
-    constructor(
-        private studentService: StudentService
-    ) { }
+  constructor(
+    private studentService: StudentService
+  ) { }
 
-    ngOnInit(): void {
-        this.tableData$ = this.studentService.getStudents$();
+  ngOnInit(): void {
+    this.tableData$ = this.studentService.getStudents$();
+  }
+
+  /**
+   * methode sortiert/toggelt die studenten liste per eigenschaft
+   * @param header
+   */
+  orderStudentListByProperty(header: string): void {
+    type T = keyof IStudent;
+
+    this.tableData$ = this.tableData$.pipe(map((data: Array<IStudent>) => {
+      if(this._isAscSorted) {
+          data.sort((a, b) => (a[<T>header] < b[<T>header]) ? -1 : ((b[<T>header] > a[<T>header]) ? 1 : 0));
+      } else {
+          data.sort((a, b) => (a[<T>header] > b[<T>header]) ? -1 : ((b[<T>header] < a[<T>header]) ? 1 : 0));
+      }
+
+      return data;
+    }));
+  }
+
+  // EVENTS
+
+  /**
+   * click event für den tabellen header
+   * @param header
+   */
+  onHeaderClick(header: string): void {
+    if(!this._currentSelected || this._currentSelected !== header) {
+      this._currentSelected = header;
+      this._isAscSorted = false;
     }
 
-    /**
-     * methode sortiert/toggelt die studenten liste per eigenschaft
-     * @param header
-     */
-    orderStudentListByProperty(header: string): void {
-        type T = keyof IStudent;
+    this.orderStudentListByProperty(this._currentSelected);
+    this._isAscSorted = !this._isAscSorted;
+  }
 
-        this.tableData$ = this.tableData$.pipe(map((data: Array<IStudent>) => {
-            if(this._isAscSorted) {
-                data.sort((a, b) => (a[<T>header] < b[<T>header]) ? -1 : ((b[<T>header] > a[<T>header]) ? 1 : 0));
-            } else {
-                data.sort((a, b) => (a[<T>header] > b[<T>header]) ? -1 : ((b[<T>header] < a[<T>header]) ? 1 : 0));
-            }
-
-            return data;
-        }));
-    }
-
-    // EVENTS
-
-    /**
-     * onclick event für den tabellen header
-     * @param header
-     */
-    onHeaderClick(header: string): void {
-        if(!this._currentSelected || this._currentSelected !== header) {
-            this._currentSelected = header;
-            this._isAscSorted = false;
-        }
-
-        this.orderStudentListByProperty(this._currentSelected);
-        this._isAscSorted = !this._isAscSorted;
-    }
-
-    /**
-     * methode liefert mat arrow als string zurück.
-     * @param header
-     * @returns
-     */
-    getArrowByHeader(header: string): string {
-        if(this._currentSelected !== header) { return ''; }
-        return this._isAscSorted ? 'arrow_upward' : 'arrow_downward';
-    }
+  /**
+   * methode liefert mat arrow als string zurück.
+   * @param header
+   * @returns
+   */
+  getArrowByHeader(header: string): string {
+    if(this._currentSelected !== header) { return ''; }
+    return this._isAscSorted ? 'arrow_upward' : 'arrow_downward';
+  }
 }
